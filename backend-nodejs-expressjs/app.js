9779baf62408dc46ee004e0220b4453f203d7fdf
SaiKiran Palle <saikiranpalle@SaiKirans-MacBook-Pro-2.local>
2023-01-15 14:28:57 -0500

    Adding different port as the app build was crashed

--- backend-nodejs-expressjs/app.js
+++ backend-nodejs-expressjs/app.js
@@ -25,4 +25,4 @@ app.use((error, req, res, next) => {
   res.json({message: error.message || 'An unknown error occurred!'});
 });
 
-app.listen(5000);
+app.listen(1234);
--- backend-nodejs-expressjs/controllers/places-controllers.js
+++ backend-nodejs-expressjs/controllers/places-controllers.js
@@ -6,7 +6,7 @@ const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
   {
-    id: 'p1',
+    id: 'p3',
     title: 'Empire State Building',
     description: 'One of the most famous sky scrapers in the world!',
     location: {
